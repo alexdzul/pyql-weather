cd982ac5b636418ac89c548ebde7486db32811a1
Alex Dzul <alexexc2@gmail.com>
2014-03-05 12:19:02 -0600

    cambios en el readme y setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,22 @@ licence = "MIT License"
 setup(name='pyql-weather',
       version=version,
       description='Yahoo Weather in Python',
+      long_description=open('README.txt').read(),
       license="MIT License",
       author=author,
       author_email=author_email,
       url='http://www.github.com/alexdzul/pyql-weather/',
       packages=['pyql','pyql.weather'],
+      classifiers=[
+            "Development Status :: 2 - Pre-Alpha",
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Natural Language :: English",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Topic :: Software Development",
+            "Topic :: Software Development :: Libraries :: Python Modules"
+            ]
      )
