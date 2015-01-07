7b496423855fb1de61bd750dcb61840144af9b0f
Alex Dzul <alexexc2@gmail.com>
2015-01-06 23:04:01 -0600

    Se agregan los errores en un archivo separado de la lógica de las consultas

---
+++ pyql/errors.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Alex Dzul'
+
+
+class InvalidYQLQueryError(Exception):
+    pass
+
+
+class YQLRequestError(Exception):
+    pass
+
+
+class MultipleValueError(Exception):
+    pass
