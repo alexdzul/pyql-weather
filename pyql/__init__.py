13c26328d2e17c6f28285b66e5729ea4c010949a
Alex Dzul <alexexc2@gmail.com>
2015-04-29 10:59:33 -0500

    Salto de versión a 0.2.2

--- pyql/__init__.py
+++ pyql/__init__.py
@@ -1,1 +1,12 @@
+from __future__ import unicode_literals
+
 __author__ = 'Alex Dzul'
+__license__ = "MIT License"
+__version__ = '0.2.2'
+__maintainer__ = "Alex Dzul"
+__email__ = "alexexc2@gmail.com"
+__status__ = "Beta"
+
+
+def get_version():
+    return __version__, __status__
--- setup.py
+++ setup.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python
 from distutils.core import setup
+from pyql import __author__, __version__, __email__, __license__, __maintainer__
 
-version = "0.2.1"
-author = "Alex Dzul"
-author_email = "alexexc2@gmail.com"
-mainteiner = "Alex Dzul"
-mainteiner_email = "alexexc2@gmail.com"
-licence = "MIT License"
 
 setup(name='pyql-weather',
-      version=version,
-      description='Yahoo Weather in Python v.%s' % version,
+      version=__version__,
+      description='Yahoo Weather in Python v.%s' % __version__,
       long_description=open('README.md').read(),
-      license="MIT License",
-      author=author,
-      author_email=author_email,
+      license=__license__,
+      author=__author__,
+      author_email=__email__,
+      maintainer=__maintainer__,
+      maintainer_email=__email__,
       url='http://www.github.com/alexdzul/pyql-weather/',
       packages=['pyql', 'pyql.weather', 'pyql.geo'],
       data_files=[('', ['README.md', 'LICENSE'])],
       keywords=['pyql', 'yahoo', 'weather'],
+      platforms='any',
       classifiers=["Development Status :: 4 - Beta",
                    "Environment :: Console",
                    "Intended Audience :: Developers",
                    "License :: OSI Approved :: MIT License",
                    "Natural Language :: English",
                    "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Programming Language :: Python :: 2",
                    "Programming Language :: Python :: 2.7",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 3.3",
+                   "Programming Language :: Python :: 3.4",
                    "Topic :: Software Development",
                    "Topic :: Software Development :: Libraries :: Python Modules",
                    ]
